5a5de02806347dcf9058dc4386752e7fa4eb345e
Kevin Abrego <Kevin@KAbrego.com>
2017-11-08 15:48:54 -0600

    more clean up
    separated html from php, increased readability

--- php/login.php
+++ php/login.php
@@ -1,81 +1,71 @@
 <?php
 session_start();
-require_once 'class.user.php';
-$user_login = new USER();
+require_once 'class.dbfunct.php';
 
-/**not verifying the user is logged in until the rest of the app is functioning**
-if($user_login->is_logged_in()!="")
-{
- $user_login->redirect('home.php');
+$admin = new DBFUNCT();
+
+//if user clicks "Submit Registration" button
+if(isset($_POST['btn-register']))
+{ 
+    $prefix = trim($_POST['acctType']);
+    $ufname = trim($_POST['txtufname']);
+    $ulname = trim($_POST['txtulname']);
+    $email = trim($_POST['txtemail']);
+    $upass = trim($_POST['txtpass']);
+    
+    $stmt = $admin->runQuery("SELECT * FROM tblUsers WHERE userEmail=:email_id");//check if email is already registered
+    $stmt->execute(array(":email_id"=>$email));
+    
+    //if email is registered then do if statement else register the user
+    if($stmt->rowCount() > 0)
+    {
+        $msg = "<div class='alert alert-error'>
+                <button class='close' data-dismiss='alert'>&times;</button>
+                <strong>Sorry!</strong>  email allready exists , Please Try another one
+                </div>";
+    }
+    else
+    {
+        if($admin->adminRegister($prefix,$ufname,$ulname,$email,$upass))//register user
+        {   
+            header('Location: ../adminview.html'); //if registration goes well
+        }
+        else //if DB throws exception
+        {
+            echo "sorry, Query could no execute...";
+        }  
+    }
 }
-*/
 
-if(isset($_POST['btn-login']))
+//if user clicks "Add Course" button
+if(isset($_POST['btn-addCourse']))
 {
- $email = trim($_POST['txtemail']);
- $upass = trim($_POST['txtupass']);
- 
- if($user_login->login($email,$upass))
- {
-   $idType = $_SESSION['userType'];
-
-   switch($idType)
-   {
-      case "000":
-         $user_login->redirect('adminview.php');
-         break;
-      case "100":
-         $user_login->redirect('staffview.php');
-         break;
-      case "900":
-         $user_login->redirect('studentview.php');
-         break;
-      default:
-         echo "Please login back in.";
-   }
- }
+    $crn = trim($_POST['txtcrn']);
+    $sub = trim($_POST['crssubj']);
+    $subnum = trim($_POST['txtsubnum']);
+    $instr = trim($_POST['txtinstrID']);
+    
+    $stmt = $admin->runQuery("SELECT * FROM Course WHERE courseNum=:crn_id");//check course is already registered
+    $stmt->execute(array(":crn_id"=>$crn));
+    
+    //if crn already exists then do if statement else register the course
+    if($stmt->rowCount() > 0)
+    {
+        $msg = "<div class='alert alert-error'>
+                <button class='close' data-dismiss='alert'>&times;</button>
+                <strong>Sorry!</strong>  Course number already exists. Please Try another one
+                </div>";
+    }
+    else
+    {
+        if($admin->addCourse($crn,$sub,$subnum,$instr))//register user
+        {   
+            header('Location: ../adminview.html'); //if registration goes well
+        }
+        else //if DB throws exception
+        {
+            echo "sorry, Query could no execute...";
+        }  
+    }
 }
 ?>
-
-<!DOCTYPE html>
-<html>
-  <head>
-    
-  </head>
-  <body id="login">
-    <div class="container">
-  <?php 
-  if(isset($_GET['inactive']))
-  {
-   ?>
-            <div class='alert alert-error'>
-    <button class='close' data-dismiss='alert'>&times;</button>
-    <strong>Sorry!</strong> This Account is not Activated Go to your Inbox and Activate it. 
-   </div>
-            <?php
-  }
-  ?>
-        <form class="form-signin" method="post">
-        <?php
-        if(isset($_GET['error']))
-  {
-   ?>
-            <div class='alert alert-success'>
-    <button class='close' data-dismiss='alert'>&times;</button>
-    <strong>Wrong Details!</strong> 
-   </div>
-            <?php
-  }
-  ?>
-        <h2 class="form-signin-heading">LogIn.</h2><hr />
-        <input type="email" class="input-block-level" placeholder="Email address" name="txtemail" required />
-        <input type="password" class="input-block-level" placeholder="Password" name="txtupass" required />
-      <hr />
-        <button class="btn btn-large btn-primary" type="submit" name="btn-login">Login</button>
-        <a href="register.php" style="float:right;" class="btn btn-large">Register</a><hr />
-        <a href="fpass.php">Lost your Password ? </a>
-      </form>
-
-    </div> <!-- /container -->
-  </body>
-</html>
